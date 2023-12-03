2b82f282b74b26e42b922da94087270d8922a2db
Milind Mishra ▲ <milind.mishra4@gmail.com>
2023-12-04 02:18:11 +0530

    📦 NEW: update hard

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,60 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  // initialise a result variable in the constructor and keep updating it after every arithmetic operation
+  constructor() {
+    this.result = 0;
+  }
+  // add: takes a number and adds it to the result
+  add(num) {
+    this.result += num;
+  }
+  // subtract: takes a number and subtracts it from the result
+  subtract(num) {
+    this.result -= num;
+  }
+  // multiply: takes a number and multiply it to the result
+  multiply(num) {
+    this.result *= num;
+  }
+  // divide: takes a number and divide it to the result
+  divide(num) {
+    this.result /= num;
+  }
+  // clear: makes the `result` variable to 0
+  clear() {
+    this.result = 0;
+  }
+  // getResult: returns the value of `result` variable
+  getResult() {
+    return this.result;
+  }
+  // calculate: takes a string expression which can take multi-arithmetic operations and give its result
+  calculate(expression) {
+    // example input: `10 +   2 *    (   6 - (4 + 1) / 2) + 7`
+    // Points to Note:
+    //   1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
+    //   2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
+    // remove all spaces
+    expression = expression.replace(/\s/g, "");
+    // check if expression has any invalid characters
+    const regex = /[^\d\+\-\*\/\(\)]/g;
+    if (regex.test(expression)) {
+      throw new Error("Invalid Expression");
+    }
+    // evaluate the expression
+    this.result = eval(expression);
+    return this.result;
+  }
+}
+
+// Test Calculator
+const calc = new Calculator();
+calc.add(10); // 10
+calc.subtract(5); // 5
+calc.multiply(2); // 10
+calc.divide(2); // 5
+console.log(calc.getResult()); // 5
 
 module.exports = Calculator;
--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,47 @@
 */
 
 class Todo {
-
+  // add(todo): adds todo to list of todos
+  constructor() {
+    this.todos = [];
+  }
+  add(todo) {
+    this.todos.push(todo);
+  }
+  // remove(indexOfTodo): remove todo from list of todos
+  remove(indexOfTodo) {
+    this.todos.splice(indexOfTodo, 1);
+  }
+  // update(index, updatedTodo): update todo at given index
+  update(index, updatedTodo) {
+    this.todos[index] = updatedTodo;
+  }
+  // getAll: returns all todos
+  getAll() {
+    return this.todos;
+  }
+  // get(indexOfTodo): returns todo at given index
+  get(indexOfTodo) {
+    return this.todos[indexOfTodo];
+  }
+  // clear: deletes all todos
+  clear() {
+    this.todos = [];
+  }
 }
+
+// test todo
+const todo = new Todo();
+todo.add("Buy Milk");
+todo.add("Buy Eggs");
+todo.add("Buy Bread");
+console.log(todo.getAll()); // [ 'Buy Milk', 'Buy Eggs', 'Buy Bread' ]
+todo.remove(1);
+console.log(todo.getAll()); // [ 'Buy Milk', 'Buy Bread' ]
+todo.update(1, "Buy Cheese");
+console.log(todo.getAll()); // [ 'Buy Milk', 'Buy Cheese' ]
+console.log(todo.get(1)); // Buy Cheese
+todo.clear();
+console.log(todo.getAll()); // []
 
 module.exports = Todo;
