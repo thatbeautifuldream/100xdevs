554d0acec26a9d6e120bd859a20052a2a921042d
Milind Mishra ▲ <milind.mishra4@gmail.com>
2023-12-03 21:21:00 +0530

    🐛 FIX: update regular function syntax and with illustrative comments

--- week-1.2-javascript-foundations/assignment/calctime.js
+++ week-1.2-javascript-foundations/assignment/calctime.js
@@ -4,9 +4,18 @@ function calculateTime() {
   setTimeout(() => {
     const end = Date.now();
     console.log(end - start, "ms");
-    // in seconds
-    console.log((end - start) / 1000, "s");
   }, 1000);
 }
 
 calculateTime();
+
+// using performance.now() instead of Date.now() gives a more precise result
+function calculateTimePrecise() {
+  const start = performance.now();
+  setTimeout(() => {
+    const end = performance.now();
+    console.log(end - start, "ms");
+  }, 1000);
+}
+
+calculateTimePrecise();
--- week-1.2-javascript-foundations/assignment/cli-clock.js
+++ week-1.2-javascript-foundations/assignment/cli-clock.js
@@ -1,11 +1,15 @@
 // Create a terminal clock (HH:MM:SS)
 
-const clock = () => {
+function clock() {
   const date = new Date();
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
   console.log(`${hours}:${minutes}:${seconds}`);
-};
+}
+
+// To run the function every second we can utilise the setInterval() method.
+// the setInterval() method takes two arguments, a function and a time in milliseconds.
+// It runs the function every time the time specified in milliseconds has passed.
 
 setInterval(clock, 1000);
