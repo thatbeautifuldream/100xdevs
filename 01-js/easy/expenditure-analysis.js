c79c34f089ae18160da60a93b64c9902f3101a8d
Milind Mishra ▲ <milind.mishra4@gmail.com>
2023-12-06 19:21:33 +0530

    📦 NEW: pass expenditure analysis

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -5,6 +5,26 @@
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
 
-function calculateTotalSpentByCategory(transactions) {}
+function calculateTotalSpentByCategory(transactions) {
+  // maintain a result array
+  const result = [];
+  // loop through the transactions array using forEach
+  transactions.forEach((transaction) => {
+    // destructuring category and price from the transaction object
+    const { category, price } = transaction;
+    // check if the category is already present in the result array
+    const categoryIndex = result.findIndex(
+      (item) => item.category === category
+    );
+    // if not present, add a new object with category and price to the result array
+    if (categoryIndex === -1) {
+      result.push({ category, totalSpent: price });
+    } else {
+      // if present, add the price to the totalSpent of the category (repeat entry)
+      result[categoryIndex].totalSpent += price;
+    }
+  });
+  return result;
+}
 
 module.exports = calculateTotalSpentByCategory;
